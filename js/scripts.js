3c2cfd8b5f9b76157e0d124dd5f3df7da7be706d
euomauricio <50330236+euomauricio@users.noreply.github.com>
2022-04-05 19:23:39 -0300

    Mauricio Final

--- js/scripts.js
+++ js/scripts.js
@@ -1,68 +1,68 @@
 const novos = [{
     id: "4d97cc39-309b-4bb0-9ae8-08c847a81e41",
-    nome: 'vaca',
-    img: 'img/juju3.jpg',
+    nome: 'Vaca Eliana',
+    img: 'img/Vaca Eliana.gif',
     quantidade: 0
 }, {
     id: "094f84b5-682c-4815-acc1-e261596fc1a7",
-    nome: 'galo',
+    nome: 'Galinha Virgínia',
     img: 'img/floquinho3.jpg',
     quantidade: 0
 }, {
     id: "ae745063-7416-4842-b897-741e9ccceda3",
-    nome: 'porco',
+    nome: 'Flash o porquinho',
     img: 'img/flash2.jpeg.',
     quantidade: 0
 }]
 
 const aves = [{
     id: "b1d1b115-aee9-494c-9704-12c164af0826",
-    nome: 'galo robô',
-    img: 'img/juju3.jpg',
+    nome: 'Galo Robô (da concorrência)',
+    img: 'img/galorobo.png',
     quantidade: 0
 }, {
     id: "3415745c-b7d2-47a2-b4d4-b0838612f5cb",
-    nome: 'galinha veridiane',
-    img: 'img/floquinho3.jpg',
+    nome: 'Galinha do Pica Pau',
+    img: 'img/galinhadopicapau.gif',
     quantidade: 0
 }, {
     id: "63797725-6210-4519-a47b-05d1d8118210",
-    nome: 'frango maguila',
-    img: 'img/flash2.jpeg.',
+    nome: 'Galo Cego',
+    img: 'img/galocego.jfif',
     quantidade: 0
 }]
 
 const suinos = [{
     id: "11d8f4fe-c73a-418d-962e-e0c58be024cf",
-    nome: 'porco arranha',
-    img: 'img/porcoarranha.png',
+    nome: 'Porco Arranha',
+    img: 'img/spiderpig.gif',
     quantidade: 0
 }, {
     id: "4876bc30-88b6-44dd-9aa4-ace7eda41fdd",
-    nome: 'porquinho Bruninho',
+    nome: 'Porquinho Bruninho',
     img: 'img/bruninho2.jpg',
     quantidade: 0
 }, {
     id: "cc8ae80f-91b2-40b3-88be-aec1ea34756e",
-    nome: 'leitão (aquele do ursinho pooh)',
+    nome: 'Leitão (aquele do Ursinho Pooh)',
     img: 'img/leitao.jpg',
     quantidade: 0
 }]
 
 const bovinos = [{
     id: "221cfba4-2f50-4c82-ae7b-a57e48422dde",
-    nome: 'boizão de gaspár',
-    img: 'img/juju3.jpg',
+    nome: 'Touro Raiz',
+    img: 'img/raiz.webp',
     quantidade: 0
 }, {
     id: "03fb1011-5ef1-422d-b7a8-b1cb77f3cdac",
-    nome: 'vaquinha mimosa',
-    img: 'img/floquinho3.jpg',
+    nome: 'Vaquinha Anchieta',
+    img: 'img/anchieta3.jpg',
     quantidade: 0
 }, {
     id: "cd169c84-b0e9-4acb-b794-b89092bf4cb5",
-    nome: 'leitão (aquele do ursinho puhf)',
-    img: 'img/flash2.jpeg.',
+    nome: 'Lobo (aquele de wallstreet)',
+    img: 'img/wallstreet.jpg.',
     quantidade: 0
 }]
 
@@ -79,7 +79,7 @@ const inicializarLoja = () => {
             <div class="single">
             <img src="${val.img}"/>
             <p>${val.nome}</p>
-        <a onclick="validarRequisicao('${val.id}')" href="#">Adicionar ao carrinho!</a></div>`
+        <button onclick="validarRequisicao('${val.id}')">Carregar no tobata</button></div>`
     }).join("")
     containerProdutos.innerHTML = stringItens
 
@@ -92,7 +92,7 @@ const inicializarLoja = () => {
             <div class="single">
             <img src="${val.img}"/>
             <p>${val.nome}</p>
-        <a onclick="validarRequisicao('${val.id}')" href="#">Adicionar ao carrinho!</a></div>`
+            <button onclick="validarRequisicao('${val.id}')">Carregar no tobata</button></div>`
     }).join("")
     containerProdutos.innerHTML = stringItens
 
@@ -105,7 +105,7 @@ const inicializarLoja = () => {
             <div class="single">
             <img src="${val.img}"/>
             <p>${val.nome}</p>
-        <a onclick="validarRequisicao('${val.id}')" href="#">Adicionar ao carrinho!</a></div>`
+            <button onclick="validarRequisicao('${val.id}')">Carregar no tobata</button></div>`
     }).join("")
     containerProdutos.innerHTML = stringItens
 
@@ -118,7 +118,7 @@ const inicializarLoja = () => {
             <div class="single">
             <img src="${val.img}"/>
             <p>${val.nome}</p>
-        <a onclick="validarRequisicao('${val.id}')" href="#">Adicionar ao carrinho!</a></div>`
+            <button onclick="validarRequisicao('${val.id}')">Carregar no tobata</button></div>`
     }).join("")
     containerProdutos.innerHTML = stringItens
 }
